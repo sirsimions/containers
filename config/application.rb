23b60364e05a68990fe26f185412a04f6711bfd9
Simion Okola <okola.simion168@gmail.com>
2024-06-14 18:42:16 +0300

    application.rb adjustment

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,15 @@ module Containerbackend
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Configure CORS to allow requests from your Netlify frontend
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins 'https://master--suharacontainerhandler.netlify.app/' # Replace with your Netlify site's URL
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
   end
 end
