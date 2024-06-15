016a0cda3045c27b00e340f9bf73b0be647d885a
Simion Okola <okola.simion168@gmail.com>
2024-06-15 14:41:27 +0300

    fixing api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
  Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://suharacontainerhandler.netlify.app/', 'https://dbcontainer.onrender.com'
+    origins 'https://suharacontainerhandler.netlify.app', 'https://dbcontainer.onrender.com'
 
     resource '*',
       headers: :any,
