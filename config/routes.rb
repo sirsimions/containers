e4378818f41b03e91ea67d9505a0d2d1b0647ab7
Simion Okola <okola.simion168@gmail.com>
2024-06-15 09:16:17 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :third_parties
   resources :authors
   resources :containerdetails
+
+  match '*path', to: 'application#cors_preflight_check', via: :options
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
